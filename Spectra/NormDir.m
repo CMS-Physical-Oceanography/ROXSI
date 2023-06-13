5e236080caa506b2ee3ef5c362c07dcc5e112430
NoahCla <136005792+NoahCla@users.noreply.github.com>
2023-06-13 11:20:23 -0400

    Add files via upload

---
+++ Spectra/NormDir.m
@@ -0,0 +1,36 @@
+function [NormWaveDir] = NormDir(SPAlat,SPAlon,SPBlat,SPBlon)
+%NORMWAVEDIR determines what incoming wave angle will be normal to the
+% shore in Monterey Bay, California
+%
+%Input Variables:
+    % SPAlat - the latitude of Point A
+    % SPAlon - the longitude of Point A
+    % SPBlat - the latitude of Point B
+    % SPBlon - the longitude of 
+%  
+%Output Variables:
+    % NormDir - the incoming normal direction of the waves 
+%    
+%Notes:
+%   - PointA should be the point furthest to the west and furthest south
+%   - PointB should be the point furthest to the east and furthest north
+%
+
+
+proj = projcrs(26944);  %utm zone: NAD83/California zone 4
+[SPAxutm,SPAyutm] = projfwd(proj,SPAlat,SPAlon);
+[SPBxutm,SPByutm] = projfwd(proj,SPBlat,SPBlon);
+
+
+ShoreVec = [SPBxutm - SPAxutm,SPByutm - SPAyutm];
+Length_ShoreVec = sqrt(ShoreVec(1)^2 + ShoreVec(2)^2);
+
+NorthVec = [0 1]; %this is just a vector pointing directly North (angle: 0 degrees)
+Length_NorthVec = 1;
+
+ShoreDir = acosd(dot(NorthVec,ShoreVec)/(Length_ShoreVec*Length_NorthVec));
+
+NormWaveDir = 270 + ShoreDir;
+
+end
+
