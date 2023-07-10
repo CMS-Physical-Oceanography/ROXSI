f28101df549376a8cae6c68781cc3e8d09d60faf
NoahCla <136005792+NoahCla@users.noreply.github.com>
2023-07-10 14:52:05 -0400

    Add files via upload

--- Spectra/function_FricFac.m
+++ Spectra/function_FricFac.m
@@ -1,171 +1,191 @@
-function[f_w] = function_FricFac(Freq,Buoy,Method)
-%FUNCTION_FRICFAC determines the friction factor based on a specified
-% frequency, buoy, and method; and a plot is created of the friction factor
-% curve as a function of frequency. 
-%
-% Notes:
-%       - if 0 is entered as the input frequency, the returned plot will
-%          not have a specific frequency and/or friction factor marked
-%
-%
+function[fw] = function_FricFac(See,ff,h,kw,a1,a2,a3,Method)
+%FUNCTION_FRICFAC determines the friction factor at each time and at each
+% frequency using Neilson's [1992] method taken from "Spectral wave
+% dissipation over a barrier reef". A method must be selected which
+% specifies which orbital velocity will be calculated.
+% 
 % Inputs:
-%        - Freq: frequency (Hz)
-%        - Buoy: the specificed buoy (Ex: 'X01')
-%        - Method: the method that should be used to determine the friction
-%                  factor (Options: representative, mean, and peak)
+%       - See: S eta eta matrix
+%       - ff: a vector of frequencies which See follows 
+%       - h: water depth vector as a function of time
+%       - kw: the roughness length (kw = 0.16 used in paper)
+%       - a1, a2, & a3: Neilson's [1992] coefficients which are typically
+%         a1 = 5.5, a2 = -0.2, and a3 = -6.3
+%       - Method: specify which bottom orbital velocity will be used to
+%          perform the calculation
+%               - The 3 Options:
+%                       - "R"/"r": representative bottom orbital velocity
+%                           method (representative used in the paper)
+%                       - "M"/"m": mean energy-weighted bottom orbital
+%                           velocity method
+%                       - "P"/"p": peak bottom orbital velocity method
+% 
 % Outputs:
-%        - fw: friction factor
-%
-%
-
-% Loading in variables from WBvariables.mat
-load('WBvariables.mat')
+%       - fw: the friction factor at each time and at each given frequency
+% 
 
 
-% Constants:
-A_omegaf = 2*pi*Freq;
-kw = 0.16; %m      %horizontal roughness length    %set to 1 in Lowe paper
-df = 0.0098;
-ff = [1:129].*df;
+%%
+%function[f_w] = function_FricFac2(See,ff,h,kw,[a1,a2,a3])
+
+% This will eventually become that function
+%       - it's not for now just for testing
+
+%% (this should be DELETED once function completed)
+% clc;clear;
+% load('WBvariables.mat')
+
+%% For now, assume:
+% Would be entered into equation:
+% See = XSee{1};
+% h = Xdepth{1};
+% df = 0.0098;
+% kw = 0.16;  %roughness length
+% ff = (1:129).*df;   %vector of frequencies contained in See
+% a1 = 5.5; a2 = -0.2; a3 = -6.3; %Nielson's constants
+% - the method should also be an input
+%%
+
+
+
+
+
+
+
+
+
+%% Preliminaries
+loops = size(See);
+df = ff(2) - ff(1);
 omegaf = 2*pi*ff;
-    % Nielsen(1992) constants:
-    a1 = 5.5;
-    a2 = -0.2;
-    a3 = -6.3;
 
-    
-    % Select the buoy to calculate for:
-switch Buoy
-%     case 'B01'        % at the time, won't work for B01
-%         See = BSee{1};
-%         h = BavgD{1};
-%         Hs = BGivenHsig{1};
-    case 'B03'
-        See = BSee{2};
-        h = BavgD{2};
-        Hs = nanmean(BGivenHsig{2});
-        Tm = nanmean(BTm{2});
-        Tp = nanmean(BTp{2});
-    case 'B05'
-        See = BSee{3};
-        h = BavgD{3};
-        Hs = nanmean(BGivenHsig{3});
-        Tm = nanmean(BTm{3});
-        Tp = nanmean(BTp{3});
-    case 'X01'
-        See = XSee{1};
-        h = XavgD{1};
-        Hs = nanmean(XGivenHsig{1});
-        Tm = nanmean(XTm{1});
-        Tp = nanmean(XTp{1});
-    case 'X03'
-        See = XSee{2};
-        h = XavgD{2};
-        Hs = nanmean(XGivenHsig{2});
-        Tm = nanmean(XTm{2});
-        Tp = nanmean(XTp{2});
-    case 'X04'
-        See = XSee{3};
-        h = XavgD{3};
-        Hs = nanmean(XGivenHsig{3});
-        Tm = nanmean(XTm{3});
-        Tp = nanmean(XTp{3});
-end
+H = 4.*sqrt(See.*df); % CHECK df GOES OUTSIDE SQRT or if there's a *4 (I THINK WHAT I HAVE IS RIGHT)
+T = 1./ff;
 
-    
-        % Select the method to use to calculate: 
-% Representative Bottom Orbital Velocity Method:
+for i = 1:loops(2)
+    Hs(i) = 4.*sqrt(sum(See(:,i)).*df);
+end   
+
 switch Method
-    case {'Representative','representative'}
-        for j = 1:129
-            f(j) = j*df;
-            T(j) = 1/f(j);
-            omega(j) = 2*pi*f(j);
-            H(j) = 4*sqrt(nansum(See(j,1:832))*df);    %times 4???
-            %H(j) = 4*sqrt(var(See(j,:)));
-            a(j) = H(j)/2;
-            [k(j)] = function_KwavecalculateSI(T(j),h);
-            u_bj(j) = H(j)*pi*f(j)/(sinh(k(j)*h)); %eqn 22 Lowe
+    
+    
+%% Representative Orbital Velocity Method
+
+    case {'R','r'}     
+        for j = 1:loops(1)
+            for i = 1:loops(2)
+                k(j,i) = function_KwavecalculateSI(T(j),h(i));  %HAVE A DIFFERENT WAY SO USERS DON'T HAVE TO DOWNLOAD TWO FUNCTIONS
+                u_b(j,i) = H(j,i)./(sinh(h(i).*k(j,i)).*ff(j));   
+            end
         end
 
-        u_bjsq = u_bj.^2;
-        %u_br = sqrt(sum(u_bjsq))
-        u_br = sqrt(sum(u_bjsq))
-        f_w = exp(a1.*(u_br./(kw*omegaf)).^a2 + a3);
-        
-        figure(200);clf;
-        plot(ff,f_w)
-        title({'Friction Factor vs Frequency using the Representative',...
-            sprintf('Bottom Orbital Velocity Method for Buoy %1s',Buoy)},...
-            'fontsize',11)
-        %xlim([0 0.5]);ylim([0 1]);
-        hold on
-        
-        if A_omegaf ~= 0
-            Af_w = exp(a1.*(u_br./(kw*A_omegaf)).^a2 + a3);
+        u_bsqr = u_b.^2;
+
+        for i = 1:loops(2)
+            u_br(i) = sqrt(sum(u_bsqr(:,i)));
+
+            for j = 1:loops(1)
+                fw(j,i) = exp(a1.*(u_br(i)./(kw.*omegaf(j))).^a2 + a3);      
+            end
         end
 
-% Mean Bottom Orbital Velocity Method:
-    case {'Mean','mean'}
-        [k] = function_KwavecalculateSI(Tm,h);
-        fm = 1/Tm;
-        u_bTm = Hs*pi*fm/(sinh(h*k))
-        f_w = exp(a1.*(u_bTm./(kw.*omegaf)).^a2 + a3);
-        
-        figure(200);clf;
-        plot(ff,f_w)
-        title({'Friction Factor vs Frequency using the Mean',...
-            sprintf('Bottom Orbital Velocity Method for Buoy %1s',Buoy)},...
-            'fontsize',11)
-        %xlim([0 0.5]);ylim([0 30]);
+
+        % max(u_br);
+
+        %       FOR CALCULATING U_BR:
+        % - Originally thought: Times 4 and df inside: max(u_br) = 55
+        % - Maybe: Times 4 and df outside: max(u_br) = 5.37
+        % - Grimes method: NOT times 4 and df inside : max(u_br) = 13.56
+
+        %       Example Plot (for representative method):
+        % - Small energy @ time 263
+        % - Medium Energy @ time 189
+        % - Large energy @ time 73
+        figure(1);clf;
+        plot(ff,fw(:,263),'g','LineWidth',2)
         hold on
+        plot(ff,fw(:,189),'b','LineWidth',2)
+        plot(ff,fw(:,73),'r','LineWidth',2)
+        xlabel('Frequency (Hz)');ylabel('Friction Factor (fw)');
+        legend('Low Energy Time','Medium Energy Time',...
+            'High Energy Time','location','northwest')
+        title('Representative Orbital Velocity Method')
+        grid on
+
+
+
+%% Peak Bottom Orbital Velocity Method
+
+    case {'P','p'}
         
-        if A_omegaf ~= 0
-            Af_w = exp(a1.*(u_bTm./(kw*A_omegaf)).^a2 + a3);
+        for i = 1:loops(2)
+            [m,indPeakFreq] = max(See(:,i));
+            Tp(i) = 1/ff(indPeakFreq);
+            fp(i) = 1/Tp(i);
         end
-       
-% Peak Bottom Orbital Velocity Method:
-    case {'Peak','peak'}
-        [k] = function_KwavecalculateSI(Tp,h);
-        fp = 1/Tp;
-        u_bTp = Hs*pi*fp/(sinh(h*k))
-        f_w = exp(a1.*(u_bTp./(kw.*omegaf)).^a2 + a3);
-        
-        figure(200);clf;
-        plot(ff,f_w)
-        title({'Friction Factor vs Frequency using the Peak',...
-            sprintf('Bottom Orbital Velocity Method for Buoy %1s',Buoy)},...
-            'fontsize',11)
-        %xlim([0 0.5]);ylim([0 3]);
+
+        for i = 1:loops(2)
+            kp(i) = function_KwavecalculateSI(Tp(i),h(i));
+            u_bTp(i) = Hs(i)/(sinh(h(i)*kp(i))*fp(i));
+
+            for j = 1:loops(1)
+                fw(j,i) = exp(a1.*(u_bTp(i)./(kw.*omegaf(j))).^a2 + a3);
+            end
+        end
+
+        %   Example Plot (for peak method):
+        figure(2);clf;
+        plot(ff,fw(:,263),'g','LineWidth',2)
         hold on
+        plot(ff,fw(:,189),'b','LineWidth',2)
+        plot(ff,fw(:,73),'r','LineWidth',2)
+        xlabel('Frequency (Hz)');ylabel('Friction Factor (fw)');
+        legend('Low Energy Time','Medium Energy Time',...
+            'High Energy Time','location','northwest')
+        title('Peak Orbital Velocity Method')
+        grid on
+
+
+
+%% Mean Energy-Weighted Bottom Orbital Velocity Method:  
+
+    case {'M','m'}
         
-        if A_omegaf ~= 0
-            Af_w = exp(a1.*(u_bTp./(kw*A_omegaf)).^a2 + a3);
+ 
+        for i = 1:loops(2)
+            m0 = trapz(ff,See(:,i),1);
+            m1 = trapz(ff,See(:,i).*ff',1);
+            Tm(i) = m0/m1;
+            fm(i) = 1/Tm(i);
         end
+
+        for i = 1:loops(2)
+            km(i) = function_KwavecalculateSI(Tm(i),h(i));
+            u_bTm(i) = Hs(i)/(sinh(h(i)*km(i))*fm(i));
+
+            for j = 1:loops(1)
+                fw(j,i) = exp(a1.*(u_bTm(i)./(kw.*omegaf(j))).^a2 + a3);
+            end
+        end
+
+        %   Example Plot (for mean method):
+        figure(3);clf;
+        plot(ff,fw(:,263),'g','LineWidth',2)
+        hold on
+        plot(ff,fw(:,189),'b','LineWidth',2)
+        plot(ff,fw(:,73),'r','LineWidth',2)
+        xlabel('Frequency (Hz)');ylabel('Friction Factor (fw)');
+        legend('Low Energy Time','Medium Energy Time',...
+            'High Energy Time','location','northwest')
+        title('Energy-Weighted Mean Orbital Velocity Method')
+        grid on
+
 end
 
-grid on
-xlabel('Frequency, {\it f} (Hz)')
-ylabel('Friction Factor {\it f_w}')
-
-if A_omegaf ~= 0
-    plot(Freq,Af_w,'.m','Markersize',15)
-    legend('Friction Factor Curve','Frequency of Interest','location','best')
-
-    ann1 = annotation('textbox',[0.14,0.8,0.37,0.07],'string',...
-        sprintf('Selected Frequency, f (Hz): %g\n',round(Freq,3)),'fontsize',10);
-    ann2 = annotation('textbox',[0.14,0.73,0.37,0.07],'string',...
-        sprintf('Friction Factor, f_w: %g\n',round(Af_w,3)),'fontsize',10);
-    ann1.BackgroundColor = 'w';
-    ann2.BackgroundColor = 'w';
-
-    f_w = Af_w
-else
-    f_w = 'Not Available'
-end
 
 end
 
-% I think something about the wave heights is wrong for mean and peak
+
+
 
 
